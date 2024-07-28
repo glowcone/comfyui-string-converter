29fd1eda91f5d374369765d321442e810e9e3962
Steph Ng <recurza@gmail.com>
2024-07-27 17:02:20 -0700

    update

--- nodes.py
+++ nodes.py
@@ -1,16 +1,4 @@
-import sys
-import os
-
-import cv2
-from PIL import Image, ImageOps
-import folder_paths
-import torch
-import numpy as np
-from io import BytesIO
-import base64
-
-
-class LoadImageFromBase64:
+class StringToFloat:
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -19,38 +7,37 @@ class LoadImageFromBase64:
             }
         }
 
-    RETURN_TYPES = ("IMAGE", "MASK")
-    FUNCTION = "load_image"
-    CATEGORY = "image"
+    RETURN_TYPES = ("FLOAT",)
+    FUNCTION = "str_to_float"
+    CATEGORY = "utils"
 
-    def convert_color(self, image):
-        if len(image.shape) > 2 and image.shape[2] >= 4:
-            return cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
-        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def str_to_float(self, data):
+        return (float(data),)
 
+class StringToInt:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "data": ("STRING", {"default": ""}),
+            }
+        }
 
-    def load_image(self, data):
-        nparr = np.frombuffer(base64.b64decode(data), np.uint8)
+    RETURN_TYPES = ("INT",)
+    FUNCTION = "str_to_int"
+    CATEGORY = "utils"
 
-        result = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-        channels = cv2.split(result)
-        if len(channels) > 3:
-            mask = channels[3].astype(np.float32) / 255.0
-            mask = torch.from_numpy(mask)
-        else:
-            mask = torch.ones(channels[0].shape, dtype=torch.float32, device="cpu")
-
-        result = self.convert_color(result)
-        result = result.astype(np.float32) / 255.0
-        image = torch.from_numpy(result)[None,]
-        return image, mask.unsqueeze(0)
+    def str_to_int(self, data):
+        return (int(data),)
 
 
 NODE_CLASS_MAPPINGS = {
-    "LoadImageFromBase64": LoadImageFromBase64,
+    "StringToInt": StringToInt,
+    "StringToFloat": StringToFloat,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LoadImageFromBase64": "Load Image From Base64",
+    "StringToInt": "Convert String To Int",
+    "StringToFloat": "Convert String To Float",
 }
 
